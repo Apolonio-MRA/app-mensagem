62993506ef9e05d84c800277eef0a50475af8a31
Moises Resende Apolonio <69828270+Apolonio-MRA@users.noreply.github.com>
2021-03-21 22:42:41 -0300

    primeiro commit

---
+++ app/src/main/java/com/moises/message/helper/constants.java
@@ -0,0 +1,13 @@
+package com.moises.message.helper;
+//tutorial criar @moises
+public class constants {
+
+    public static final String SHARED_PREF_NAME = "simplifiedcodingchat";
+    public static final String USER_ID = "userid";
+    public static final String USER_NAME = "username";
+    public static final String USER_EMAIL= "useremail";
+    public static final String IS_LOGGED_IN= "is_logged_in";
+
+    public static final String PUSH_NOTIFICATION = "pushnotification";
+    public static final int NOTIFICATION_ID = 235;
+}
